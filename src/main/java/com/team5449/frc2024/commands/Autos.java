66a788aca794404fa8de079808b551f63b131458
tomtan <tomtan20060613@outlook.com>
2024-09-11 13:59:29 +0800

    Init Commit

---
+++ src/main/java/com/team5449/frc2024/commands/Autos.java
@@ -0,0 +1,23 @@
+// Copyright (c) 2024 FRC 5449
+// http://github.com/FRC-team5449
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.team5449.frc2024.commands;
+
+import com.team5449.frc2024.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static Command exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
